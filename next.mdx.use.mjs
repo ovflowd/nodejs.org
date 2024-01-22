d2b067bf4b1fa533be81e7065a28286acf993e8c
Michele Riva <ciao@micheleriva.it>
2024-01-22 22:44:05 +0100

    refactor: moves components and hooks into the correct folder structure

--- next.mdx.use.mjs
+++ next.mdx.use.mjs
@@ -11,7 +11,7 @@ import UpcomingEvents from './components/MDX/Calendar/UpcomingEvents';
 import UpcomingSummits from './components/MDX/Calendar/UpcomingSummits';
 import MDXCodeBox from './components/MDX/CodeBox';
 import MDXCodeTabs from './components/MDX/CodeTabs';
-import SearchPage from './components/SearchPage';
+import SearchPage from './components/MDX/SearchPage';
 import WithBadge from './components/withBadge';
 import WithBanner from './components/withBanner';
 import WithNodeRelease from './components/withNodeRelease';
